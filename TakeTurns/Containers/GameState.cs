f99c49d7ef9cba22263d41d961a7bac061d5e79d
Isaac hawn <ihawn.pl15@gmail.comm>
2022-05-13 19:29:46 -0500

    base classes

---
+++ TakeTurns/Containers/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeTurns.Containers
+{
+    public abstract class GameState<GameSpace, AgentType>
+    {
+        public virtual GameSpace Space { get; set; }
+        public virtual IList<AgentType> Agents { get; set; }
+        public virtual AgentType CurrentAgent { get; set; }
+    }
+}
---
+++ TakeTurns/Containers/MinimaxContainers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeTurns.Containers
+{
+    public class MinimaxInput : GameState<object, object>
+    {
+        public MinimaxInput(object space, IList<object> moves, object agent)
+        {
+            Space = space;
+            Moves = moves;
+            Agent = agent;
+        }
+
+        public MinimaxInput(object space)
+        {
+            Space = space;
+        }
+    }
+
+    public class MinimaxOutput : GameState<object, object>
+    {
+        public int MinimaxEvaluation { get; set; }
+
+        public MinimaxOutput()
+        {
+            MinimaxEvaluation = 0;
+            Space = null;
+            Moves = new List<object>();
+            Agent = new object();
+        }
+
+        public MinimaxOutput(bool isMin, object space)
+        {
+            MinimaxEvaluation = isMin ? int.MaxValue : int.MinValue;
+            Space = space;
+            Moves = new List<object>();
+            Agent = new object();
+        }
+
+        public MinimaxOutput(int evaluation, object space, List<object> moves, object agent)
+        {
+            MinimaxEvaluation = evaluation;
+            Space = space;
+            Moves = Moves;
+            Agent = agent;
+        }
+    }
+}
